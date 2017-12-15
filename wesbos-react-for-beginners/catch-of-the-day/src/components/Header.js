54d060b4f8ca0cf4e851dbece56e47f231787688
Xavian Ang <xavianaxw@gmail.com>
2017-12-15 11:48:37 +1100

    #7 Passing data with props

--- wesbos-react-for-beginners/catch-of-the-day/src/components/Header.js
+++ wesbos-react-for-beginners/catch-of-the-day/src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <p>Header</p>
+      <header className="top">
+        <h1>Catch of the Day</h1>
+        <h3 className="tagline">{ this.props.tagline }</h3>
+      </header>
     )
   }
 }
