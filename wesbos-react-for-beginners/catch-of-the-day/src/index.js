346f3ec2d7a3ddfa3d3d532c18a9c91f2b3e162d
Xavian Ang <xavianaxw@gmail.com>
2017-12-15 11:45:31 +1100

    lesson 6

--- wesbos-react-for-beginners/catch-of-the-day/src/index.js
+++ wesbos-react-for-beginners/catch-of-the-day/src/index.js
@@ -3,9 +3,10 @@ import React from 'react';
 import { render } from 'react-dom';
 
 // Components
+import App from './components/App';
 import StorePicker from './components/StorePicker';
 
 // CSS
 import './css/style.css';
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
