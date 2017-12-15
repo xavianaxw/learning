b30181daa31c9b6d7fc6fd7a9a7ef83722152c62
Xavian Ang <xavianaxw@gmail.com>
2017-12-15 11:30:26 +1100

    add StorePicker

--- wesbos-react-for-beginners/catch-of-the-day/src/index.js
+++ wesbos-react-for-beginners/catch-of-the-day/src/index.js
@@ -1,1 +1,11 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// Components
+import StorePicker from './components/StorePicker';
+
+// CSS
+import './css/style.css';
+
+render(<StorePicker/>, document.querySelector('#main'));
