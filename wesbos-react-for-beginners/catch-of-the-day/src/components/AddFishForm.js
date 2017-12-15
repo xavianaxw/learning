a7f4e569691e851b660e39df3112f3d0c8936fb1
Xavian Ang <xavianaxw@gmail.com>
2017-12-15 16:40:16 +1100

    #13 hook up submit handler

--- wesbos-react-for-beginners/catch-of-the-day/src/components/AddFishForm.js
+++ wesbos-react-for-beginners/catch-of-the-day/src/components/AddFishForm.js
@@ -1,17 +1,57 @@
 import React from "react";
 
 class AddFishForm extends React.Component {
+  addFish(e) {
+    e.preventDefault();
+    const fish = {
+      name: this.name.value,
+      price: this.price.value,
+      status: this.status.value,
+      desc: this.desc.value,
+      image: this.image.value,
+    };
+
+    console.log(fish);
+  }
+
   render() {
     return (
-      <form className="fish-edit">
-        <input type="text" placeholder="Fish Name" />
-        <input type="text" placeholder="Fish Price" />
-        <select>
+      <form className="fish-edit" onSubmit={e => this.addFish(e)}>
+        <input
+          ref={input => {
+            this.name = input;
+          }}
+          type="text"
+          placeholder="Fish Name"
+        />
+        <input
+          ref={input => {
+            this.price = input;
+          }}
+          type="text"
+          placeholder="Fish Price"
+        />
+        <select
+          ref={input => {
+            this.status = input;
+          }}
+        >
           <option value="Fresh">Fresh</option>
           <option value="Sold Out">Sold Out</option>
         </select>
-        <textarea placeholder="Fish Desc" />
-        <input type="text" placeholder="Fish Image" />
+        <textarea
+          ref={input => {
+            this.desc = input;
+          }}
+          placeholder="Fish Desc"
+        />
+        <input
+          ref={input => {
+            this.image = input;
+          }}
+          type="text"
+          placeholder="Fish Image"
+        />
         <button type="submit">Add Fish</button>
       </form>
     );
